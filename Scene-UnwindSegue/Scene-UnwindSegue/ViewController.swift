f05a41dc0dc156972c447e4f6b15c3c5428b5e8c
이재백 <jaebaek@ijaebaeg-ui-MacBook-Pro.local>
2016-12-13 23:36:13 +0900

    15.5.3 Unwind 기본: UIStoryboard 타입의 인자값을 입력받도록 정의한 함수를, Exit 버튼 (Dock Bar 내에 위치)과 연결하여 Unwind를 구현

---
+++ Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 이재백 on 2016. 12. 13..
+//  Copyright © 2016년 JaeBaek Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
