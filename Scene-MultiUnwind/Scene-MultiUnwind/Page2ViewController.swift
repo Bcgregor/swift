37c65801e9897f3ba2ec9600c47867182a88137b
이재백 <jaebaek@ijaebaeg-ui-MacBook-Pro.local>
2016-12-14 12:02:27 +0900

    15.5.3 화면 복귀: Multi Unwind, UIStoryboardSegue 객체를 인자값으로 받는 메소드를 구현하면 시스템에서 Unwind 함수로 인식, Exit 아이콘에서 버튼 객체와 세그로 연결할 때 해당 메소드를 연결하면 Unwind Segue를 손쉽게 구현가능.

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 이재백 on 2016. 12. 14..
+//  Copyright © 2016년 JaeBaek Lee. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController : UIViewController {
+    
+    @IBAction func gotoPage2(_sender: UIStoryboardSegue) {
+        
+    }
+}
---
+++ Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 이재백 on 2016. 12. 13..
+//  Copyright © 2016년 JaeBaek Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
