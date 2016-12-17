5b379ba47912473bf760b75d95b58e4072bf4366
이재백 <jaebaek@ijaebaeg-ui-MacBook-Pro.local>
2016-12-17 14:34:52 +0900

    16.2.1 (왜 또 올라가지 않은 파일이라고?) 프레젠테이션 방식으로 화면 전환하면서 값을 전달하기 (+네비게이션 바 이용 전환, 메뉴얼 세그, 액션 세그 다 이용)

--- SubmitValue/SubmitValue/ResultViewController.swift
+++ SubmitValue/SubmitValue/ResultViewController.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2016년 JaeBaek Lee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ResultViewController : UIViewController {
+    
+    @IBOutlet var resultEmail: UILabel!
+    
+    @IBOutlet var resultUpdate: UILabel!
+    
+    @IBOutlet var resultInterval: UILabel!
+    
+    // 아울렛 변수에 값을 바로 대입할 수 없다.
+    // 외부 객체에서 직접 참조할 수 없도록 제한되어 있다.
+    // 따라서 이전 뷰 컨트롤러에서 변수를 받아오기 위한 변수를 따로 선언해야 한다.
+    var paramEmail: String = ""
+    
+    var paramUpdate: Bool = false
+    
+    var paramInterval: Double = 0
+    
+    
+    override func viewDidLoad() {
+        self.resultEmail.text = paramEmail
+        self.resultUpdate.text = (self.paramUpdate == true ? "자동갱신" : "자동갱신 안함")
+        self.resultInterval.text = "\(Int(paramInterval))분 마다 갱신"
+    }
+    
+    @IBAction func onBack(_ sender: Any) {
+         self.presentingViewController?.dismiss(animated: true)
+        // self.navigationController?.popViewController(animated: true)
+    }
+    
+}
--- SubmitValue/SubmitValue/ViewController.swift
+++ SubmitValue/SubmitValue/ViewController.swift
@@ -20,6 +20,79 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 이메일 주소를 입력받는 텍스트 필드
+    @IBOutlet var email: UITextField!
+    
+    // 자동 갱신 여부를 설정하는 스위치
+    @IBOutlet var isUpdate: UISwitch!
+    
+    // 갱신 여부를 표시하는 레이블
+    @IBOutlet var interval: UIStepper!
+    
+    // 자동 갱신 여부를 표시하는 레이블
+    @IBOutlet var isUpdateText: UILabel!
+    
+    // 갱신주기를 텍스트로 표시하는 레이블
+    @IBOutlet var intervalText: UILabel!
+    
+    
+    // 자동 갱신 여부가 바뀔 때마다 호출되는 메소드
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            self.isUpdateText.text = "갱신함"
+        } else {
+            self.isUpdateText.text = "갱신하지 않음"
+        }
+    }
+    
+    
+    @IBAction func onStepper(_ sender: UIStepper) {
+        
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value)분 마다"
+        
+    }
+ 
+    // 이 녀석의 연결을 끊고, 안 끊고 아래의 IBAction 메소드를 대용으로 쓰고 안 쓰고!
+    @IBAction func onSubmit(_ sender: Any) {
+        
+        // VC2의 인스턴스 생성
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else {
+            return
+        }
+        
+        // 값을 전달
+        rvc.paramEmail = self.email.text!
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+        
+        // 화면 이동
+        // 하단에 '제출' 버튼으로 구현할 경우
+        // self.present(rvc, animated: true)
+        
+        // 상단에 네비게이션 바로 구현할 경우
+         self.navigationController?.pushViewController(rvc, animated: true)
+        
+    }
 
+    @IBAction func onPerformSegue(_ sender: Any) {
+        self.performSegue(withIdentifier: "ActionSegue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // 목적지 뷰 컨트롤러 인스턴스 읽어오기
+        let dest = segue.destination
+        
+        guard let rvc = dest as? ResultViewController else {
+            return
+        }
+        
+        // 값 전달
+        rvc.paramEmail = self.email.text!
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+    }
+    
+    
 }
-
