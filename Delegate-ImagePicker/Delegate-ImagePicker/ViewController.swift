35e65814bf57bf3eec8009a20a5a131214ba35e2
이재백 <jaebaek@ijaebaeg-ui-MacBook-Pro.local>
2016-12-28 16:25:39 +0900

    19.3 프로토타입 셀을 다루는 두가지 방법, 태그 속성값을 이용하는 방법과 커스텀 클래스를 새로 만들고 연결하여 다루는 방법.

--- Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBAction func pick(_ sender: Any) {
+        
+        // 이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 On
+        
+        // 추가된 부분. 델리게이트 지정
+        picker.delegate = self
+        
+        // 이미지 피커 컨트롤러 실행
+        self.present(picker, animated: false)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // 이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false) { () in
+            
+            // 창이 닫힌 후에 일어나야 하는 일들이므로 !! completion을 통해 구현한다.
+            // 알림창 호출
+            let alert = UIAlertController(title: "",
+                                            message: "이미지 선택이 취소되었습니다.",
+                                            preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+            
     }
-
-
+        
+    // 이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false) { () in
+            
+            // 이미지를 이미지 뷰에 표시
+            let img = info[UIImagePickerControllerEditedImage] as? UIImage
+            self.imgView.image = img
+        }
+    }
+        
+        
 }
+    
+    
+
+
 
---
+++ MyMovieChart/MyMovieChart/MovieCell.swift
@@ -0,0 +1,9 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 이재백 on 2016. 12. 28..
+//  Copyright © 2016년 JaeBaek Lee. All rights reserved.
+//
+
+import Foundation
